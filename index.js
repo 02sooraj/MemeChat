e083fa5635504f249751726a99ba28ded456a577
02sooraj <128619003+02sooraj@users.noreply.github.com>
2023-03-25 18:01:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const firebaseConfig = {
   
         var title = document.createElement('h1')
         title.setAttribute('id', 'title')
-        title.textContent = 'ChatHUB'
+        title.textContent = 'Meme Chat'
   
         title_inner_container.append(title)
         title_container.append(title_inner_container)
